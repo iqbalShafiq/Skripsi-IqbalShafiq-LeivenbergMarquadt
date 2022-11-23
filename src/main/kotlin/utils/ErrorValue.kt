e6f84bc2547d47df3a72118c2ed25543920ca398
iqbalShafiq <iqbal.syafiq.rozaan@gmail.com>
2022-11-23 10:29:08 +0700

    feat(Error): menghitung error
    - menghitung error output layer
    - menghitung mse
    
    h6mCtzM5/19-menghitung-error-output-layer
    4t2pgHOX/4-menghitung-mse

--- src/main/kotlin/utils/ErrorValue.kt
+++ src/main/kotlin/utils/ErrorValue.kt
@@ -1,11 +1,30 @@
 package utils
 
 object ErrorValue {
-    fun calculateOutputLayerError() {
 
+    /**
+     * Menghitung error pada output layer berdasarkan value pada output dan target
+     * @param target = target kelas (label) dengan tipe data double
+     * @param outputValue = value dari output layer dengan tipe data double
+     * @return mengembalikan error = target - outputValue dengan tipe data double
+     */
+    fun calculateOutputLayerError(target: Double, outputValue: Double): Double {
+        return target - outputValue
     }
 
-    fun calculateMeanSquaredError() {
+    /**
+     * Menghitung Mean Squared Error (MSE)
+     * @param errorList = list error dari output layer dengan tipe data list of double
+     * @return Sum(power(error, 2)) / n dengan tipe data double
+     */
+    fun calculateMeanSquaredError(errorList: List<Double>): Double {
+        var sumOfSquaredError = 0.0
+        val n = errorList.size
 
+        errorList.forEach { error ->
+            sumOfSquaredError += (error * error)
+        }
+
+        return sumOfSquaredError / n
     }
 }
