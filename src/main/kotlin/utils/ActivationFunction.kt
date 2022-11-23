5ca8b1673cbe07c2914639ed1a2f73d224992c14
iqbalShafiq <iqbal.syafiq.rozaan@gmail.com>
2022-11-23 10:14:20 +0700

    feat(Matrix): menghitung turunan pertama fungsi sigmoid
    4KsmSWaU/18-menghitung-turunan-pertama-fungsi-aktivasi

--- src/main/kotlin/utils/ActivationFunction.kt
+++ src/main/kotlin/utils/ActivationFunction.kt
@@ -5,9 +5,17 @@ import kotlin.math.exp
 object ActivationFunction {
 
     /**
-     * Menghitung fungsi aktivasi berdasarkan net yang
+     * Menghitung fungsi aktivasi sigmoid berdasarkan net
      * @param net = input dengan tipe data double
      * @return mengembalikan nilai dengan tipe data double
      */
-    fun calculateActivationFunction(net: Double): Double = 1 / (1 + exp(net))
+    fun calculateSigmoidFunction(net: Double): Double = 1 / (1 + exp(net))
+
+    /**
+     * Menghitung turunan pertama fungsi aktivasi sigmoid berdasarkan net
+     * @param net = input dengan tipe data double
+     * @return mengembalikan nilai dengan tipe data double
+     */
+    fun calculateDerivativeSigmoidFunction(net: Double): Double =
+        calculateSigmoidFunction(net) * (1 - calculateSigmoidFunction(net))
 }
